bb7418d7adbf2cb98e325df558fc1ab1a1fdac3d
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2020-07-08 00:36:16 +0530

    component structure

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-class Dishdetail extends Component {
+class DishDetail extends Component {
   constructor(props) {
     super(props);
     // console.log(props);
-    this.state = {};
+    this.state = {
+      selectedDish:null
+    };
   }
 
   renderDish(dish) {
@@ -34,7 +36,7 @@ class Dishdetail extends Component {
               year: "numeric",
               month: "long",
               day: "2-digit",
-            }).format(new Date(comments.date))}
+            }).format(new Date(Date.parse(comments.date)))}
           </p>
         </li>
       );
@@ -46,20 +48,24 @@ class Dishdetail extends Component {
       </div>
     );
   }
-
+ 
   render() {
-    if (this.props.selectedDish != null)
+    console.log("dishDetail "+ this.props.dish);
+    if (this.props.dish != null)
       return (
+        <div className="container">
         <div className="row">
           <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.selectedDish)}
+            {this.renderDish(this.props.dish)}
           </div>
           <div className="col-12 col-md-5 m-3">
-            {this.renderComment(this.props.selectedDish.comments)}
+            {this.renderComment(this.props.dish.comments)}
           </div>
+        </div>
         </div>
       );
     else return <div> </div>;
   }
 }
-export default Dishdetail;
+export default DishDetail;
+      
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,25 +1,13 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-import Dishdetail from "./DishdetailComponent";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
     console.log("menu constructor");
-    this.state = {
-      selectedDish: null,
-    };
   }
   componentDidMount() {
     console.log("componetDidMount created");
-  }
-
-  //method to invoke on click/select of any dish, i.e., change the selectedDish to current selected dish
-  onDishSelect(dish) {
-    console.log("dish selected");
-    this.setState({
-      selectedDish: dish,
-    });
   }
 
   //render will return the view
@@ -28,7 +16,7 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.onDishSelect(dish)}>
+          <Card onClick={()=> this.props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle> {dish.name} </CardTitle>
@@ -41,7 +29,6 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row"> {menu} </div>
-        <Dishdetail selectedDish={this.state.selectedDish} />
       </div>
     );
   }
